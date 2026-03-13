b7079415d9817668690d0ae39663f14e2e2aa585
omer guttman <guttman99@gmail.com>
2026-03-13 16:12:24 +0200

    Remove unused imports from Japan.js

--- my-site/src/pages/Japan.js
+++ my-site/src/pages/Japan.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, useMap, Polyline, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
